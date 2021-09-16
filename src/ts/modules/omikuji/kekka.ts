d04a7a58b8233e1032f651c79d49d61bb9031158
konno <konno@buddying.jp>
2021-09-16 17:57:18 +0900

    ejs整理

--- src/ts/modules/omikuji/kekka.ts
+++ src/ts/modules/omikuji/kekka.ts
@@ -5,5 +5,5 @@ const unsei: string[] = Unsei.unsei;
 // 結果
 export let kekka = (): string => {
     let nmb: number = Math.floor(Math.random() * unsei.length );
-    return unsei[nmb]; 
+    return unsei[nmb];
 }
--- src/ts/modules/omikuji/omikuji.ts
+++ src/ts/modules/omikuji/omikuji.ts
@@ -4,4 +4,5 @@ import { kekka } from "./kekka";
 export let omikuji = (): void => {
     let massage: string = 'あなたの運勢は「' + kekka() + '」です！！';
     alert(massage);
+    console.log("確認");
 }

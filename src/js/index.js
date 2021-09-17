fad721b0e5cd73dd96d2582874eb52e692935c96
konno <konno@buddying.jp>
2021-09-17 15:45:31 +0900

    js追加

---
+++ src/js/index.js
@@ -0,0 +1,5 @@
+import PageTop from './lib/page-top'
+
+$(function() {
+    console.log("確認");
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,33 @@
+// module.exports = {
+//     mode: 'production', // development or production
+//     entry: './src/ts/script.ts',
+//     output: {
+//         path: `${__dirname}/dest/js`,
+//         filename: "script.js"
+//       },
+//     module: {
+//       rules: [
+//         {
+//           test: /\.ts$/,
+//           use: 'ts-loader',
+//         },
+//       ],
+//     },
+//     resolve: {
+//       extensions: [
+//         '.ts', '.js',
+//       ],
+//     },
+// };
+
+const path = require('path');
+
 module.exports = {
-    mode: 'production', // development or production
-    entry: './src/ts/script.ts',
-    output: {
-        path: `${__dirname}/dest/js`,
-        filename: "script.js"
-      },
-    module: {
-      rules: [
-        {
-          test: /\.ts$/,
-          use: 'ts-loader',
-        },
-      ],
-    },
-    resolve: {
-      extensions: [
-        '.ts', '.js',
-      ],
-    },
-  };
+  entry: './src/js/index.js',
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'dest/js/'),
+  },
+  mode: 'production',
+  watch: true,
+};

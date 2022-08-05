8b6be53c5a88c49cea6593949d19d0c86bba7eee
konno <konno@buddying.jp>
2022-08-05 17:59:11 +0900

    環境更新

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
-# EJS-TEMPLATE
+# ejs-template
+
+## 開発
+
+### ディレクトリ構造
+
+```txt
+├── public  # ドキュメントルート
+└── src     # コンパイル前ソース
+```
+
+#### html,css,js
+`/src`内で元ファイルを編集してコンパイル。
+
+### ローカル開発
+
+##### 依存ファイルインストール
+```
+npm install
+```
+
+##### 開発
+```
+npx gulp
+http://localhost:3000/
+```
+
+### 参考
+
+https://zenn.dev/yurukei20/articles/f1f9b248fcf18b
+
+### GitHub Pages
+
+https://konno1614.github.io/ejs-template/public/
--- gulpfile.js
+++ gulpfile.js
@@ -1,162 +1,185 @@
-const gulp = require('gulp');
-const rename = require('gulp-rename');
-const ejs = require('gulp-ejs');
-const fs = require('fs');
-const replace = require('gulp-replace');
-const sass = require('gulp-sass');
-const webpackStream = require('webpack-stream');
-const webpack = require('webpack');
-const webpackConfig = require('./webpack.config');
-const browserSync = require('browser-sync');
-const plumber = require('gulp-plumber');
-const imagemin = require('gulp-imagemin');
-const mozjpeg = require('imagemin-mozjpeg');
-const pngquant = require('imagemin-pngquant');
-const del = require('del');
+//gulp本体
+const gulp = require("gulp");
 
+// ejs
+const fs = require("fs");
+const ejs = require("gulp-ejs");
+const rename = require("gulp-rename");
+// prettier
+const prettier = require("gulp-prettier");
+// const csscomb = require("gulp-csscomb");
+// scss Dart Sass はSass公式が推奨 @use構文などが使える
+const sass = require("gulp-dart-sass");
+// css 縮小化
+const purgecss = require("gulp-purgecss");
+const cleancss = require("gulp-clean-css");
+// js 縮小化
+const uglify = require("gulp-uglify");
+// img 縮小化
+const imagemin = require("gulp-imagemin");
+// エラーが発生しても強制終了させない
+const plumber = require("gulp-plumber");
+// エラー発生時のアラート出力
+const notify = require("gulp-notify");
+//ブラウザリロード
+const browserSync = require("browser-sync");
 
-// ファイルパス：コンパイル前
-// const srcJsonFiles = './src/json/**/*.json';
-// const srcDataJson = './src/json/data.json';
-const srcEjsFiles = './src/ejs/**/*.ejs';
-const srcEjsPartial = '!./src/ejs/**/_*.ejs';
-const srcScssFiles = './src/scss/**/*.scss';
-// const srcTsFiles = './src/ts/**/*.ts';
-const srcImgFiles = './src/img/**/*'
-const srcImgFileType = '{jpg,jpeg,png,gif,svg}';
+// 入出力するフォルダを指定
+const srcBase = "./src";
+const docsBase = "./public";
 
-
-// ファイルパス：コンパイル後
-const destDir = './public/';
-const destFiles = './public/**/*';
-const destHtmlFiles = './public/*.html';
-const destIndexHtml = 'index.html';
-const destCssDir = './public/css';
-const destCssFiles = './public/css/*.css';
-const destJsDir = './public/js';
-const destJSFiles = './public/js/*.js';
-const destImtDir = './public/img';
-const destImgFiles = './public/img/*';
-
-
-// EJSコンパイル
-const compileEjs = (done) => {
-    // const data = JSON.parse(fs.readFileSync(srcDataJson));
-    gulp.src([srcEjsFiles, srcEjsPartial])
-        .pipe(plumber())
-        // .pipe(ejs(data))
-        .pipe(ejs({}, {}, { ext: '.html' }))
-        .pipe(rename({ extname: '.html' }))
-        .pipe(replace(/^[ \t]*\n/gmi, ''))
-        .pipe(gulp.dest(destDir));
-    done();
+const srcPath = {
+    scss: srcBase + "/asset/sass/**/*.scss",
+    js: srcBase + "/asset/js/*.js",
+    img: srcBase + "/asset/img/**",
+    html: srcBase + "/**/*.html",
+    ejs: [srcBase + "/**/*.ejs", "!" + srcBase + "/**/_*.ejs"],
 };
 
+const docsPath = {
+    css: docsBase + "/asset/css/",
+    js: docsBase + "/asset/js/",
+    img: docsBase + "/asset/img/",
+    html: docsBase + "/",
+    ejs: docsBase + "/",
+};
 
-// sassコンパイル
-const compileSass = (done) => {
-    gulp.src(srcScssFiles)
-        .pipe(sass({
-                    outputStyle: 'expanded'
-				})
+/**
+ * sass
+ */
+const cssSass = () => {
+    return (
+        gulp
+            .src(srcPath.scss, {
+                sourcemaps: true,
+            })
+            .pipe(
+                //エラーが出ても処理を止めない
+                plumber({
+                    errorHandler: notify.onError("Error:<%= error.message %>"),
+                })
             )
-		.on('error', sass.logError)
-        .pipe(gulp.dest(destCssDir));
-    done();
+            .pipe(sass({ outputStyle: "expanded" })) //指定できるキー expanded compressed
+            .pipe(
+                purgecss({
+                    content: [
+                        "./src/**/*.html",
+                        "./src/**/*.ejs",
+                        "./src/**/*.js",
+                    ], // src()のファイルで使用される可能性のあるファイルを全て指定
+                })
+            )
+            // .pipe(csscomb()) // csscombでCSSの順序指定
+            .pipe(cleancss())
+            .pipe(gulp.dest(docsPath.css, { sourcemaps: "./" })) //コンパイル先
+            .pipe(browserSync.stream())
+            .pipe(
+                notify({
+                    message: "Sassをコンパイルしました！",
+                    onLast: true,
+                })
+            )
+    );
 };
 
-
-// TypeScriptをwebpackでバンドル
-const bundleWebpack = (done) => {
-    webpackStream(webpackConfig, webpack)
-        .pipe(gulp.dest(destJsDir));
-    done();
+/**
+ * js
+ */
+const js = () => {
+    return gulp
+        .src(srcPath.js)
+        .pipe(
+            //エラーが出ても処理を止めない
+            plumber({
+                errorHandler: notify.onError("Error:<%= error.message %>"),
+            })
+        )
+        .pipe(uglify())
+        .pipe(gulp.dest(docsPath.js))
+        .pipe(browserSync.stream());
 };
 
-
-// リロードするhtml
-const reloadFile = (done) => {
-    browserSync.init({
-        server : {
-            baseDir : destDir,
-            index : destIndexHtml,
-		},
-    });
-    done();
+/**
+ * img
+ */
+const img = () => {
+    return gulp
+        .src(srcPath.img)
+        .pipe(
+            //エラーが出ても処理を止めない
+            plumber({
+                errorHandler: notify.onError("Error:<%= error.message %>"),
+            })
+        )
+        .pipe(
+            imagemin([
+                imagemin.svgo(),
+                imagemin.optipng(),
+                imagemin.gifsicle({ optimizationLevel: 3 }),
+            ])
+        )
+        .pipe(gulp.dest(docsPath.img))
+        .pipe(browserSync.stream());
 };
 
+/**
+ * html
+ */
+const html = () => {
+    return gulp.src(srcPath.html).pipe(gulp.dest(docsPath.html));
+};
 
-// リロード設定
-const reloadBrowser = (done) => {
+/**
+ * ejs
+ */
+const ejsHtml = () => {
+    const json = JSON.parse(fs.readFileSync("./ejs-config.json"));
+    return gulp
+        .src(srcPath.ejs)
+        .pipe(plumber())
+        .pipe(ejs(json, { ext: ".html" }))
+        .pipe(rename({ extname: ".html" }))
+        .pipe(prettier())
+        .pipe(gulp.dest(docsPath.ejs));
+};
+
+/**
+ * ローカルサーバー立ち上げ
+ */
+const browserSyncFunc = () => {
+    browserSync.init(browserSyncOption);
+};
+
+const browserSyncOption = {
+    server: docsBase,
+};
+
+/**
+ * リロード
+ */
+const browserSyncReload = (done) => {
     browserSync.reload();
     done();
 };
 
-
-// 画像圧縮
-const minifyImage = (done) => {
-    gulp.src(srcImgFiles + srcImgFileType)
-        .pipe(imagemin(
-        [
-            pngquant({ quality: '65-80', speed: 1 }),
-            mozjpeg({ quality: 80 }),
-            imagemin.svgo(),
-            imagemin.gifsicle()
-        ]
-        ))
-        .pipe(gulp.dest(destImtDir));
-    done();
+/**
+ * ファイル監視 ファイルの変更を検知したら、browserSyncReloadでreloadメソッドを呼び出す
+ * series 順番に実行
+ * watch('監視するファイル',処理)
+ */
+const watchFiles = () => {
+    gulp.watch(srcPath.scss, gulp.series(cssSass));
+    gulp.watch(srcPath.js, gulp.series(js));
+    gulp.watch(srcPath.img, gulp.series(img));
+    gulp.watch(srcPath.html, gulp.series(html, browserSyncReload));
+    gulp.watch(srcPath.ejs, gulp.series(ejsHtml, browserSyncReload));
 };
 
-
-// destフォルダのファイル削除
-const clean = (done) => {
-    del([destFiles, '!' + destCssDir, '!' + destJsDir, '!' + destImtDir]);
-    done();
-};
-
-
-// HTMLファイル削除
-const htmlClean = (done) => {
-    del([destHtmlFiles]);
-    done();
-};
-
-
-// 画像ファイル削除
-const imgClean = (done) => {
-    del([destImgFiles]);
-    done();
-};
-
-
-// タスク化
-exports.compileEjs = compileEjs;
-exports.compileSass = compileSass;
-exports.bundleWebpack = bundleWebpack;
-exports.reloadFile = reloadFile;
-exports.reloadBrowser = reloadBrowser;
-exports.minifyImage = minifyImage;
-exports.clean = clean;
-exports.htmlClean = htmlClean;
-exports.imgClean = imgClean;
-
-
-// 監視ファイル
-const watchFiles = (done) => {
-    // gulp.watch([srcEjsFiles, srcJsonFiles], gulp.series(htmlClean, compileEjs));
-    gulp.watch(destHtmlFiles, reloadBrowser);
-    gulp.watch(srcScssFiles, compileSass);
-    gulp.watch(destCssFiles, reloadBrowser);
-    // gulp.watch([srcTsFiles, srcJsonFiles], bundleWebpack);
-    gulp.watch(destJSFiles, reloadBrowser);
-    gulp.watch(srcImgFiles, gulp.series(imgClean, minifyImage));
-    gulp.watch(destImgFiles, reloadBrowser);
-    done();
-};
-
-
-// タスク実行
+/**
+ * seriesは「順番」に実行
+ * parallelは並列で実行
+ */
 exports.default = gulp.series(
-    clean, watchFiles, reloadFile, compileEjs, compileSass, bundleWebpack, minifyImage
+    gulp.parallel(html, ejsHtml, img, js, cssSass),
+    gulp.parallel(watchFiles, browserSyncFunc)
 );

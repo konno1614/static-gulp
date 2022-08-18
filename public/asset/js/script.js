5d5ba056f0f7abb378e1bfd734ea3089dd64e6b8
konno <konno@buddying.jp>
2022-08-18 16:04:42 +0900

    更新

--- public/asset/js/script.js
+++ public/asset/js/script.js
@@ -1,3 +1,2 @@
-$(function(){for(i=1;i<=10;i++)$(".masterList").append("<li>追加テキスト"+i+"</li>")});
 $(function(){console.log("Nav")});
 $(function(){console.log("Script")}),window.onload=function(){console.log("Script after")};
--- src/asset/js/master.js
+++
@@ -1,5 +0,0 @@
-$(function() {
-    for(i=1;i<=10;i++){
-        $('.masterList').append('<li>追加テキスト'+i+'</li>')
-    }
-});
